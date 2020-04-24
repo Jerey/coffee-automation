8064bf89b66f785d9dbdee8b10351d73c60f6765
Anton Jerey <antonjerey@gmail.com>
2020-04-24 14:56:04 +0200

    Automatic coffee grinder.
    Initial working version of the coffee grinder automation.

---
+++ README.md
@@ -0,0 +1,93 @@
+# Coffee Grinder Control
+
+Automate your coffee grinder with the help of a D1 mini (or any similar ESP based device) and (optional) a scale.
+
+## Motivation
+
+I enjoy a good cup of coffee. For that, I have bought myself a coffee grinder (Graef CM 800) to start with. This grinder has a switch with three positions: Off, on and grind while pressing a small button.
+
+Working from home during the corona pandemic made me drink more coffee at home and I started to get annoyed of the manual labour of controlling the amount of grinded beans. Inspired by [Roman Seidl's repository](https://github.com/romanseidl/MillControl), I decided to automatize my mill.
+
+## Goal
+
+The outcome of this project shall be able to keep the grinder on for a given time or until a target weight is reached. Further, it should not require a lot of hardware to rebuild.
+
+## Solution
+
+Since I had some D1 mini laying around at home, I chose it as the brain of this project. Since I have already used **mqtt** with my [weatherstation](https://github.com/aaj07/weatherstation-client), I also decided to control the mill via mqtt topics. Further I had also played around with **Node-Red** and **Node-Red-Dashboard**, which made it really easy to quickly create a control panel for the grinder. See [the sample of my dashboard](#control-the-grinder).
+
+<img src="/documentation/Prototype.jpg" width="400">
+
+### MQTT Topics
+
+The topics defined for this project are split up in two types: incoming and outgoing. All topics start with `grinder/`.
+
+#### Incoming
+
+These are the topics used to control the mill. All start with the topic `in/`.
+
+- `grinder/in/start`: Starts the grinder with the time given in the payload. If no time is specified in the payload, the last set time is used.
+- `grinder/in/setGrindingTime`: Sets the time, which will be used if start is called without a time specified.
+- `grinder/in/tare`: Tares the scale.
+- `grinder/in/automatic`: Starts the grinder with the stored default value. This also **tares** the scale before starting.
+
+#### Outgoing
+
+In order to get updates from the grinder, the following topics are published by the D1 mini. These can be visualized by subscribing to them.
+
+- `grinder/out/started`: Indicates, that the grinder was started. The payload holds the reason, why it was started.
+- `grinder/out/currentSetGrindingTime`: Published, when the stored time was changed. The payload indicates the new time (in `ms`).
+- `grinder/out/finished`: Whenever the current grinding is finished, this topic is published along with the set grinding time.
+- `grinder/out/automaticFinished`: If you start an automatic grinding session, this indicates, that it was finished. The payload contains the grinded weight.
+- `grinder/out/weight`: Published on a regular basis to indicate the current weight on the scale.
+
+## Setup
+
+If you want to setup this project at home, you will need following things:
+
+- D1 mini (or similar ESP device)
+- Relay controllable by the D1 mini and supporting the electricity of your mill
+- A load cell and a load cell amplifier (I used the **HX711**)
+- A mqtt broker
+
+### Flashing
+
+Initially, you will have to flash this software via USB, but after that, OTA is available and already configured.
+
+### Variables in the code
+
+- `hostName`: Defines the `MQTT` and `OTA` hostname. This is also the name defined in the `platformio.ini`-file at `upload_port`.
+- `relay`: Defines the GPIO port of the relay data signal.
+- `scaleData`: Defines the GPIO port of the scale data signal.
+- `scaleClk`: Defines the GPIO port of the scale clock signal.
+- `scaleCalibrationFactor`: Defines the scale calibration factor. See [scale calibration](#scale-calibration).
+- `desiredGrams`: The default desired grams. This is the target weight you desire when automatically grinding.
+- `scaleUpdateTime`: The frequency of updates published for the current weight.
+- `ssidAP`: The name of the access point opened up by the D1 mini, when it has no WiFi to connect to. If you connect to the D1 mini via this access point, it will allow you to provide credentials to a WiFi of your choice within reach.
+- `passwordAP`: The password needed to connect to the access point of your D1 mini, when it is opened.
+- `mqttBroker`: The ip address of your mqtt broker.
+- `grindingTime`: The default grinding time. It is used, when start is called without any payload.
+
+### Schematic
+
+:warning: Do not work on the mill's electricity, if you have no clue of what you are doing. :warning:
+
+Therefore I will just provide a simple schematic. The relay must be connected to the grinder and the load cell amplifier (here **HX711**) to the load cell.
+
+<img src="/documentation/CoffeeGrinder.svg" width="400">
+
+### Scale calibration
+
+The scale must be calibrated. To retrieve the calibration factor, I used the sparkfun [calibration](https://github.com/sparkfun/HX711-Load-Cell-Amplifier/blob/master/firmware/SparkFun_HX711_Calibration/SparkFun_HX711_Calibration.ino) sample. I wanted grams, so I checked that this factor already respects that.
+This retrieved calibration factor has to be set in the `program.cpp`.
+
+### Control the grinder
+
+Last but not least: You will need a way to publish the MQTT commands. There are several options for this:
+
+- Command line (e.g. `mosquitto_pub -d -t grinder/in/automatic -m ""`).
+- Node Red Dash Board
+
+<img src="/documentation/Node-RED%20Dashboard.png" width="400">
+
+- ...
---
+++ src/program.cpp
@@ -0,0 +1,236 @@
+#include <Arduino.h>
+#include <ArduinoOTA.h>
+#include <HX711.h>
+#include <PubSubClient.h>
+#include <WiFiManager.h>
+
+//----------- Hostname -----------
+constexpr const char* hostName = "CoffeeGrinder";
+
+//----------- In/Out -----------
+constexpr int relay = 15;
+
+//----------- Scale -----------
+constexpr int scaleData = 4;
+constexpr int scaleClk = 5;
+HX711 scale;
+constexpr float scaleCalibrationFactor = 418;
+constexpr float desiredGrams = 16.8;
+unsigned long lastScaleUpdate;
+unsigned int scaleUpdateTime = 500;
+
+//----------- AP -----------
+constexpr const char* ssidAP = "AutoConnectAP";
+constexpr const char* passwordAP = "password";
+
+//----------- MQTT -----------
+constexpr const char* mqttBroker = "192.168.178.100";
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+char mqttBuffer[64];
+unsigned int grindingTime = 0 * 1000;
+unsigned long grindingStartedTime;
+
+constexpr const char* topicInSetGrindingTime = "grinder/in/setGrindingTime";
+constexpr const char* topicInStart = "grinder/in/start";
+constexpr const char* topicInTare = "grinder/in/tare";
+constexpr const char* topicInAutomatic = "grinder/in/automatic";
+
+constexpr const char* topicOutStarted = "grinder/out/started";
+constexpr const char* topicOutCurrentSetGrindingTime =
+    "grinder/out/currentSetGrindingTime";
+constexpr const char* topicOutFinished = "grinder/out/finished";
+constexpr const char* topicOutAutomaticFinished =
+    "grinder/out/automaticFinished";
+constexpr const char* topicOutCurrentWeight = "grinder/out/weight";
+
+//----------- Grinding Flags -----------
+bool grindingOngoing = false;
+bool automaticGrindingOngoing = false;
+
+/**
+ * Function to connect to a wifi. It waits until a connection is established.
+ */
+void connectToWifi() {
+  WiFiManager wifiManager;
+  wifiManager.setBreakAfterConfig(true);
+  wifiManager.autoConnect(ssidAP, passwordAP);
+}
+
+void subscribeToMqttTopics() {
+  mqttClient.subscribe(topicInSetGrindingTime);
+  mqttClient.subscribe(topicInStart);
+  mqttClient.subscribe(topicInTare);
+  mqttClient.subscribe(topicInAutomatic);
+}
+
+template <typename valueType>
+void printTopicAndValue(const char* topic, valueType value) {
+  Serial.print(topic);
+  Serial.print(": ");
+  Serial.println(value);
+}
+
+void reconnectToMqttBroker() {
+  while (not mqttClient.connected()) {
+    Serial.print("Reconnecting...");
+    if (not mqttClient.connect(hostName)) {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" retrying in 5 seconds");
+      delay(5000);
+    }
+    subscribeToMqttTopics();
+  }
+}
+
+template <typename valueType>
+void publishMqttTopicAndValue(const char* topic, valueType value) {
+  reconnectToMqttBroker();
+  mqttClient.loop();
+  printTopicAndValue(topic, value);
+  auto resultOfPublish = mqttClient.publish(topic, mqttBuffer);
+  if (not resultOfPublish) {
+    Serial.print("Failed to publish the topic '");
+    Serial.print(topic);
+    Serial.print("'");
+  }
+}
+
+float getCurrentWeightAndPublish() {
+  auto currentWeight = scale.get_units(3);
+  if ((millis() - lastScaleUpdate) > scaleUpdateTime) {
+    lastScaleUpdate = millis();
+    snprintf(mqttBuffer, sizeof mqttBuffer, "%.1f", currentWeight);
+    publishMqttTopicAndValue(topicOutCurrentWeight, currentWeight);
+  }
+  return currentWeight;
+}
+
+unsigned int getIntFromPayload(byte* payload, unsigned int length) {
+  payload[length] = '\0';
+  String s = String((char*)payload);
+  return s.toInt();
+}
+
+void setGrindingTime(unsigned int timeToGrind) {
+  grindingTime = timeToGrind;
+  snprintf(mqttBuffer, sizeof mqttBuffer, "%d", grindingTime);
+  publishMqttTopicAndValue(topicOutCurrentSetGrindingTime, grindingTime);
+}
+
+void startGrinding(const char* startTriggerOrigin,
+                   unsigned int timeToGrind = grindingTime) {
+  grindingOngoing = true;
+  grindingStartedTime = millis();
+  setGrindingTime(timeToGrind);
+  strcpy(mqttBuffer, startTriggerOrigin);
+  publishMqttTopicAndValue(topicOutStarted, startTriggerOrigin);
+  digitalWrite(relay, HIGH);
+}
+
+/**
+ * @brief MQTT Callback
+ */
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Received message [");
+  Serial.print(topic);
+  Serial.println("] ");
+
+  if (strcmp(topic, topicInSetGrindingTime) == 0) {
+    setGrindingTime(getIntFromPayload(payload, length));
+    Serial.print("-> ");
+    Serial.println(grindingTime);
+  } else if (strcmp(topic, topicInStart) == 0) {
+    if (length > 0) {
+      startGrinding("callbackWithTime", getIntFromPayload(payload, length));
+    } else {
+      startGrinding("callbackWithoutTime");
+    }
+  } else if (strcmp(topic, topicInTare) == 0) {
+    scale.tare(5);
+  } else if (strcmp(topic, topicInAutomatic) == 0) {
+    scale.tare(5);
+    getCurrentWeightAndPublish();
+    automaticGrindingOngoing = true;
+    startGrinding("automaticGrinding", 6000);
+  } else {
+    Serial.print("Unhandled topic '");
+    Serial.print(topic);
+    Serial.println("'!");
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, LOW);
+  connectToWifi();
+  ArduinoOTA.setHostname(hostName);
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_FS
+      type = "filesystem";
+    }
+  });
+  ArduinoOTA.onEnd([]() { Serial.println("\nEnd"); });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  mqttClient.setServer(mqttBroker, 1883);
+  mqttClient.setCallback(callback);
+  reconnectToMqttBroker();
+
+  scale.begin(scaleData, scaleClk);
+  scale.set_scale(scaleCalibrationFactor);
+  scale.tare();
+}
+
+void loop() {
+  mqttClient.loop();
+  reconnectToMqttBroker();
+
+  getCurrentWeightAndPublish();
+  if (grindingOngoing) {
+    if ((millis() - grindingStartedTime) > grindingTime) {
+      grindingOngoing = false;
+      digitalWrite(relay, LOW);
+      snprintf(mqttBuffer, sizeof mqttBuffer, "%d", grindingTime);
+      publishMqttTopicAndValue(topicOutFinished, grindingTime);
+    }
+    if (not grindingOngoing && automaticGrindingOngoing) {
+      delay(500);
+      auto currentWeight = getCurrentWeightAndPublish();
+      if (desiredGrams <= currentWeight) {
+        automaticGrindingOngoing = false;
+        snprintf(mqttBuffer, sizeof mqttBuffer, "%.1f", currentWeight);
+        publishMqttTopicAndValue(topicOutAutomaticFinished, currentWeight);
+      } else {
+        startGrinding("automaticGrinding", 150);
+      }
+    }
+  } else {
+    ArduinoOTA.handle();
+  }
+}
