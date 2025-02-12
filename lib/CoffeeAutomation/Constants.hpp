fc7221523279b1d0f4f7d931d9fb346fc4eb8ec7
Jerey <anton@jerey.at>
2025-02-12 20:42:01 +0100

    Add WiFi configuration constants and MQTT settings to Constants.hpp

--- lib/CoffeeAutomation/Constants.hpp
+++ lib/CoffeeAutomation/Constants.hpp
@@ -1,9 +1,28 @@
 #pragma once
 
+// The access point name of the ESP when it is in wifi configuration mode.
 static constexpr const char* ssidAP = "AutoConnectAP";
+// The default password to the access point of the ESP when it is in wifi
+// configuration mode.
 static constexpr const char* passwordAP = "password";
+// A timeout of the wifi configuration portal in seconds.
+// After this timeout, the ESP will restart and try to connect to the wifi
+// again.
 static constexpr unsigned int configPortalTimeout = 600;
 
+// The mqtt broker IP address.
+static constexpr const char* mqttBroker = "192.168.178.100";
+// The hostname for the grinder for
+// - the mqtt connection
+// - firmware updates over the air (OTA)
+static constexpr const char* hostName = "CoffeeGrinder";
+
+// A threshold for the travel distance from the grinder to the scale.
+static constexpr float thresholdTargetGrams = 1.0f;
+// How often a scale update can happen in ms.
+static constexpr unsigned int scaleUpdateTime = 500;
+
+// MQTT topics
 static constexpr const char* topicInSetGrindingTime =
     "grinder/in/setGrindingTime";
 static constexpr const char* topicInStart = "grinder/in/start";
@@ -17,9 +36,3 @@ static constexpr const char* topicOutFinished = "grinder/out/finished";
 static constexpr const char* topicOutAutomaticFinished =
     "grinder/out/automaticFinished";
 static constexpr const char* topicOutCurrentWeight = "grinder/out/weight";
-
-static constexpr const char* mqttBroker = "192.168.178.100";
-static constexpr const char* hostName = "CoffeeGrinder";
-
-static constexpr float thresholdTargetGrams = 1.0f;
-static constexpr unsigned int scaleUpdateTime = 500;
